970bb88f572a5beb6a6136767cfc3ddef7e64ebb
RASHIKACHAUHAN24 <106758047+RASHIKACHAUHAN24@users.noreply.github.com>
2023-07-11 19:38:54 +0530

    Add files via upload

---
+++ PROJECT - 113.py
@@ -0,0 +1,91 @@
+import sys
+import time
+import random
+
+import os
+import shutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = "C:/Users/pihuc/Downloads"
+to_dir = "C:/Users/pihuc/Desktop/Downloaded_Files"
+
+dir_tree = {
+    "Image_Files": ['.jpg', '.jpeg', '.png', '.gif', '.jfif'],
+    "Video_Files": ['.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.mp4', '.m4p', '.m4v', '.avi', '.mov'],
+    "Document_Files": ['.ppt', '.xls', '.csv', '.pdf', '.txt'],
+    "Setup_Files": ['.exe', '.bin', '.cmd', '.msi', '.dmg']
+}
+
+class FileMovementHandler(FileSystemEventHandler):
+
+    def on_created(self, event):
+        print(f"Hey!,{event.src_path} has been created !")
+
+    def on_deleted(self, event):
+        print(f"Oops!Someone deleted{event.src_path} !")
+
+    def on_modified(self, event):
+        print(f"Hey there!,{event.src_path} has been modified !")
+
+    def on_moved(self, event):
+        print(f"Someone moved {event.src_path} to {event.dest_path}  !")
+       
+        name, extension = os.path.splitext(event.src_path)
+       
+        time.sleep(1)
+
+        for key, value in dir_tree.items():
+            time.sleep(1)
+
+            if extension in value:
+
+                file_name = os.path.basename(event.src_path)
+               
+                print("Downloaded " + file_name)
+
+                path1 = from_dir + '/' + file_name
+                path2 = to_dir + '/' + key
+                path3 = to_dir + '/' + key + '/' + file_name
+
+                if os.path.exists(path2):
+
+                    print("Directory Exists...")
+                    print("Moving " + file_name + "....")
+                    shutil.move(path1, path3)
+                    time.sleep(1)
+
+                else:
+                    print("Making Directory...")
+                    os.makedirs(path2)
+                    print("Moving " + file_name + "....")
+                    shutil.move(path1, path3)
+                    time.sleep(1)
+
+
+# Initialize Event Handler Class
+event_handler = FileMovementHandler()
+
+
+# Initialize Observer
+observer = Observer()
+
+# Schedule the Observer
+observer.schedule(event_handler, from_dir, recursive=True)
+
+
+# Start the Observer
+observer.start()
+
+#Student Activity2
+
+try:
+    while True:
+        time.sleep(2)
+        print("running...")
+except KeyboardInterrupt:
+    print("Stopped!")
+    observer.stop()
+
+    
